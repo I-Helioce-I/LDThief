f2a383fd72ada4b36e24d3fc2c400abb76db4266
Helioce <88891978+I-Helioce-I@users.noreply.github.com>
2024-03-01 15:36:28 +0100

    StartDetection

--- LDThief/Assets/Scripts/Detection.cs
+++ LDThief/Assets/Scripts/Detection.cs
@@ -9,20 +9,49 @@ public enum Entity
 }
 public class Detection : MonoBehaviour
 {
-    [SerializeField] private Entity entity = Entity.Enemy;
+    [SerializeField] Entity currentEntity = Entity.Enemy;
+    [SerializeField] List<Detection> entities = new List<Detection>();
+
+    [SerializeField] Detection currentTarget;
+
+    [Header("Stats")]
+    [SerializeField] float radius;
+
 
     private void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        
+        Detect();
+        CheckForSound();
     }
 
-    
 
+    public void Detect()
+    {
+        entities.Clear();
 
+        Collider[] _collidersArray = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider collider in _collidersArray)
+        {
+            if (collider.TryGetComponent(out Detection detection))
+            {
+                if (detection.currentEntity != currentEntity)
+                {
+                    entities.Add(detection);
+                }
+
+            }
+        }
+    }
+
+    public void CheckForSound()
+    {
+
+    }
 
 }
